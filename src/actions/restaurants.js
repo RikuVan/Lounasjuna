a8013210775521f8a29d1a9b3822b60c2cf93f71
Richard Van Camp <richard.vancamp@gofore.com>
2017-03-31 09:20:59 +0300

    fix: sort restaurants by rating

--- src/actions/restaurants.js
+++ src/actions/restaurants.js
@@ -3,10 +3,12 @@ import {fbUrls} from '../dataApi'
 
 export const FETCH_RESTAURANTS = 'FETCH_RESTAURANTS'
 
+const sortByRating = (a, b) => (b.rating || 0) - (a.rating || 0);
+
 export const fetchRestaurants = () => {
   const handler = resp => {
-    //get rid of null values
-    const data = resp.data.filter(x => x);
+    //get rid of null values & sort by rating
+    const data = resp.data.filter(x => x).sort(sortByRating);
     return {...resp, data}
   }
   return apiGet({
